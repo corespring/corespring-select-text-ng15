e6e96a05aedc98a4244c8274b294023c4cae0d5b
rbokel <rbokel.github@qlod.org>
2016-12-01 12:54:22 +0100

    move to corespring

---
+++ docs/demo/controllers/entry.js
@@ -0,0 +1,4 @@
+
+         exports['corespring-select-text-ng15'] = require('corespring-select-text-ng15-controller');
+         exports['corespring-select-text-ng15'].version = '../node_modules/corespring-select-text-ng15/controller';
+         
---
+++ src/demo.js
@@ -0,0 +1,3 @@
+import Element from './index.js';
+
+document.registerElement('corespring-select-text-ng15', Element);
