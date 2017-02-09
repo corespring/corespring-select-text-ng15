adce2b35aa4d6a186b10a7f46b27b8e0d862d993
Ed Eustace <ed.eustace@gmail.com>
2017-02-09 10:20:03 +0000

    remove redundant assets

--- docs/demo/controllers/entry.js
+++
@@ -1,4 +0,0 @@
-
-         exports['corespring-select-text-ng15'] = require('corespring-select-text-ng15-controller');
-         exports['corespring-select-text-ng15'].version = '../node_modules/corespring-select-text-ng15/controller';
-         
